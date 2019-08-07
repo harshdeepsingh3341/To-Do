b290270fb268c55a20c6ddd58d59ff4732211f53
Harshdeep <harshdeep.singh@tothenew.com>
2019-08-07 11:53:19 +0530

    app constants

---
+++ src/client/src/constants.js
@@ -0,0 +1,19 @@
+export const STYLE_CONSTANTS = {
+	PRIMARY_COLOR:'#009688',
+	SECONDARY_COLOR: '#78909C',
+	BACKGROUND_COLOR: '#fafafa'
+};
+
+export const SIDEBAR_MENUS = [
+	{
+		id: 1,
+		name: 'inbox',
+
+	}
+];
+
+export const API_VERSION = {
+	VERSION_1: 'v1'
+};
+
+export const API_ENDPONT = `http://localhost:8080/api/${API_VERSION.VERSION_1}`;
