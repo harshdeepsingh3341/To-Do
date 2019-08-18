19e2a7cbd981a5d0f5aceee0665a099a5243094c
Harshdeep Singh <harshdeep.singh@tothenew.com>
2019-08-18 10:37:22 +0530

    rest api status code and their respective messages object added and encryption key for password encryption and decryption added

--- src/server/config/development.config.js
+++ src/server/config/development.config.js
@@ -2,5 +2,8 @@ module.exports = {
 	MONGODB_URL: process.env.NODE_ENV === 'production' ?
 		'':
 		'mongodb://localhost:27017/todoGraphql',
-	ENCRYPTION_SECRET:"###qwerty@todolist##@@author:&&Harshdeep&*%Singh###"
+	ENCRYPTION_SECRET:"###qwerty@todolist##@@author:&&Harshdeep&*%Singh###",
+	API_STATUS_CODE_MESSAGES: {
+		"200": "success"
+	}
 };
